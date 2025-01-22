ccee7c78ef5f815c956083647a6e62a9efd07e00
IkerHurCab <ikerhurtadocaballo@gmail.com>
2025-01-22 23:07:05 +0100

    updated stone skin

--- resources/Utils.js
+++ resources/Utils.js
@@ -2,14 +2,14 @@ import Plataforma from './Plataforma.js';
 
 class Utils {
 
-    static staircase(scene, positionX, positionY, height, step = 1, inverted = false) {
+    static staircase(scene, texture, positionX, positionY, height, step = 1, inverted = false) {
         for (let i = 0; i < height; i++) {
-            const platform = new Plataforma(scene, 'single_platform', 18, 18);
+            const platform = new Plataforma(scene, texture, 18, 18);
 
-            if (!inverted) 
+            if (!inverted)
                 platform.createGrid(500 + 54 * positionX + 54 * i * step, window.innerHeight - 54 * positionY - (54 * i), 1, height * step - i * step);
 
-            else   
+            else
                 platform.createGrid(500 + 54 * positionX + 54 * step, window.innerHeight - 54 * positionY - (54 * i), 1, height * step - i * step);
             scene.platforms.push(platform.getPlatforms());
         }
@@ -25,48 +25,45 @@ class Utils {
         scene.platforms.push(platform.getPlatforms());
     }
 
-    static rectangle(scene, positionX, positionY, width, height) {
-        const platform = new Plataforma(scene, 'single_platform', 18, 18);
+    static rectangle(scene, texture,positionX, positionY, width, height) {
+        const platform = new Plataforma(scene, texture, 18, 18);
         platform.createGrid(500 + 54 * positionX, window.innerHeight - 54 * positionY, width, height, 3);
         scene.platforms.push(platform.getPlatforms());
     }
 
-    static unfilledRectangle(scene, positionX, positionY, width, height) {
-        const platform = new Plataforma(scene, 'single_platform', 18, 18);
-        
+    static unfilledRectangle(scene, texture,positionX, positionY, width, height) {
+        const platform = new Plataforma(scene, texture, 18, 18);
+
         platform.createGrid(500 + 54 * positionX, window.innerHeight - 54 * positionY, 1, width, 3);
         platform.createGrid(500 + 54 * positionX, window.innerHeight - 54 * (positionY - height + 1), 1, width, 3);
-        
         platform.createGrid(500 + 54 * positionX, window.innerHeight - 54 * positionY, height, 1, 3);
-        
         platform.createGrid(500 + 54 * (positionX + width - 1), window.innerHeight - 54 * positionY, height, 1, 3);
-        
         scene.platforms.push(platform.getPlatforms());
     }
 
     static grass(scene, positionX, positionY, height, width) {
         const platform = new Plataforma(scene, 'single_grass_middle', 18, 18);
         const platformIni = new Plataforma(scene, 'single_grass_left', 18, 18);
-        const platformEnd = new Plataforma(scene, 'single_grass_right', 18, 18);    
-        
+        const platformEnd = new Plataforma(scene, 'single_grass_right', 18, 18);
+
         for (let i = 0; i < height; i++) {
             if (i == height - 1) {
-            platformIni.createGrid(500 + 54 * positionX, window.innerHeight - 54 * (positionY + i), 1, 1, 3);
-            platform.createGrid(500 + 54 * (positionX + 1), window.innerHeight - 54 * (positionY + i), 1, width - 2, 3);
-            platformEnd.createGrid(500 + 54 * (positionX + width - 1), window.innerHeight - 54 * (positionY + i), 1, 1, 3);
+                platformIni.createGrid(500 + 54 * positionX, window.innerHeight - 54 * (positionY + i), 1, 1, 3);
+                platform.createGrid(500 + 54 * (positionX + 1), window.innerHeight - 54 * (positionY + i), 1, width - 2, 3);
+                platformEnd.createGrid(500 + 54 * (positionX + width - 1), window.innerHeight - 54 * (positionY + i), 1, 1, 3);
             } else {
-            const platformDirt = new Plataforma(scene, 'single_dirt_middle', 18, 18);
-            const platformIni = new Plataforma(scene, 'single_dirt_left', 18, 18);
-            const platformEnd = new Plataforma(scene, 'single_dirt_right', 18, 18);
-            platformIni.createGrid(500 + 54 * positionX, window.innerHeight - 54 * (positionY + i), 1, 1, 3);
-            platformDirt.createGrid(500 + 54 * (positionX + 1), window.innerHeight - 54 * (positionY + i), 1, width - 2, 3);
-            platformEnd.createGrid(500 + 54 * (positionX + width - 1), window.innerHeight - 54 * (positionY + i), 1, 1, 3);
-            scene.platforms.push(platformDirt.getPlatforms());
-            scene.platforms.push(platformIni.getPlatforms());
-            scene.platforms.push(platformEnd.getPlatforms());
+                const platformDirt = new Plataforma(scene, 'single_dirt_middle', 18, 18);
+                const platformIni = new Plataforma(scene, 'single_dirt_left', 18, 18);
+                const platformEnd = new Plataforma(scene, 'single_dirt_right', 18, 18);
+                platformIni.createGrid(500 + 54 * positionX, window.innerHeight - 54 * (positionY + i), 1, 1, 3);
+                platformDirt.createGrid(500 + 54 * (positionX + 1), window.innerHeight - 54 * (positionY + i), 1, width - 2, 3);
+                platformEnd.createGrid(500 + 54 * (positionX + width - 1), window.innerHeight - 54 * (positionY + i), 1, 1, 3);
+                scene.platforms.push(platformDirt.getPlatforms());
+                scene.platforms.push(platformIni.getPlatforms());
+                scene.platforms.push(platformEnd.getPlatforms());
             }
         }
-        
+
         scene.platforms.push(platform.getPlatforms());
         scene.platforms.push(platformIni.getPlatforms());
         scene.platforms.push(platformEnd.getPlatforms());
